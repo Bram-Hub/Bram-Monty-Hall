e5be5ac8badcac6f3a6f0cea94f2afa29fcd6451
hsueha <hsueha@rpi.edu>
2021-06-13 15:18:37 -0400

    making navigation for the pages

--- resources/views/database.blade.php
+++ resources/views/database.blade.php
@@ -2,6 +2,37 @@
 <html>
     <head>
         <title>Monty Hall - Database Page</title>
+        <!--<link rel="stylesheet" href="/css/navigation.css">-->
+        <style>
+        	.navigationBar{
+				font: 15px Arial, sans-serif;
+			}
+			.navItems{
+				background-color: white;
+				border: none;
+				color: blue;
+				padding: 15px 32px;
+				text-align: center;
+				text-decoration: none;
+				display: inline-block;
+				font-size: 16px;
+
+			}
+        </style>
     </head>
+    <div class="navigationBar">
+    	<span class="navItems">
+    		<a class="navItems" href="/play">Play</a>
+    	</span>
+    	<span class="navItems">
+    		<a class="navItems" href="/research">Research/Simulate</a>
+    	</span>
+    	<span class="navItems">
+    		<a class="navItems" href="/database">Database</a>
+    	</span>
+    	<span class="navItems">
+    		<a class="navItems" href="/education">Education</a>
+    	</span>
+    </div>
 	<h1>Monty Hall - Database Page</h1>
 </html>
