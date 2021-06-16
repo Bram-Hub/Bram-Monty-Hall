5c67be542da6338b0afa524124366f21cd218e2d
zwirvinpeng <zwirvinpeng99@gmail.com>
2021-06-16 02:50:23 -0400

    Play page layout/image file
    Play page layout first draft;
    Added an image file.

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -4,6 +4,15 @@
         <title>Monty Hall - Play Page</title>
     </head>
     <x-navigation/>
-    <h1>Monty Hall - Play Page</h1>
+    <h1>Let's Make a Deal!!</h1>
+    <body>
+        <p>Behind one of the door is a car!</p>
+        <p>Behind other doors are goats...</p>
+        <button name="door1" type="button" value="bar" onclick="" > <img src='images/door1.png' width=200 height=400> </button>
+        <button name="door2" type="button" value="bar" onclick="" > <img src='images/door2.png' width=200 height=400> </button>
+        <button name="door3" type="button" value="bar" onclick="" > <img src='images/door3.png' width=200 height=400> </button>
+        <img src='images/monty.jpeg' width=200 height=150>
+        <p>Open a door by clicking on it</p>
+    </body>
     <x-footer/>
 </html>
