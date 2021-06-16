30c1c21ef068bbf18f33cf92d006ff9a488c2c4f
litala123 <61203683+litala123@users.noreply.github.com>
2021-06-16 15:45:25 -0400

    Updated tabs to be 4 spaces in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,14 @@
 window.openVariants = function (evt, cityName) {
-  var x = document.getElementsByClassName("monty");
-  for (var i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(cityName).style.display = "block";
-  var y = document.getElementsByClassName("variantsButton");
-  for (var i = 0; i < y.length; i++) {
-    y[i].className = y[i].className.replace(" active", "");
-  }
-  evt.currentTarget.className += " active";
+    var x = document.getElementsByClassName("monty");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(cityName).style.display = "block";
+    var y = document.getElementsByClassName("variantsButton");
+    for (var i = 0; i < y.length; i++) {
+        y[i].className = y[i].className.replace(" active", "");
+    }
+    evt.currentTarget.className += " active";
 }
 //line below doesn't work :p
 document.getElementById("defaultOpen").click();
