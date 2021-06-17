2a9e0c637c4f470d5021eef5a8b1ee5c045ad351
hsueha <hsueha@rpi.edu>
2021-06-16 23:27:51 -0400

    Added initial sections of about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,13 +2,30 @@
 <html>
 <head>
     <title>Monty Hall - About Page</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link href="{{ URL::asset('css/about.css') }}" rel="stylesheet"/>
 </head>
 <body>
     <x-navigation/>
-    
+    <div class="pageBody">
+        <h1 class="pageTitle">About Us</h1>
+        <p>Lorem ipsum</p>
+        <div>
+            stuff
+        </div>
+        <h1 class="pageTitle">How to Play</h1>
+        <p>Lorem ipsum</p>
+        <div>
+            stuff
+        </div>
+        <h1 class="pageTitle">The Team</h1>
+        <p>Lorem ipsum</p>
+        <div>
+            stuff
+        </div>
+    </div>
     <x-footer/>
 </body>
 </html>
