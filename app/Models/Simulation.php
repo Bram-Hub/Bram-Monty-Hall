aafa4259eb8fc7650a3460cdd65a802f59b51463
almog-co <cohenalmog905@gmail.com>
2021-07-27 11:25:04 -0400

    Updated Simulation add function

--- app/Models/Simulation.php
+++ app/Models/Simulation.php
@@ -5,33 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Exception;
 
 class Simulation extends Model
 {
     use HasFactory;
     use SoftDeletes;
 
+    protected $fillable = ['behavior_matrix', 'wins_switched', 'total_switches', 'total_losses', 'total_wins', 'total_simulations'];
+
     public function monty()
     {
         return $this->belongsTo(Monty::class);
-    }
-
-    public function readSimulation(){// check all results on the table
-        return $this -> all();
-    }
-
-    public function oneSimulation($data, $arr){// check certain Simulation result on the table
-        return $this -> where($data, $arr) -> get();
-    }
-
-    public function delSimulation($data){// delete certain Simulation
-        $simulation = $this -> where($data);
-        return $simulation -> delete();
-    }
-
-    public function updSimulation($data, $list, $arr){// update certain Simulation
-        $simulation = $this -> where($data, $list);
-        return $simulation -> update($arr);
     }
 
     /**
@@ -39,31 +24,41 @@ class Simulation extends Model
      * 
      * data contains all columns we should add it on the table for a single Simulation object.
      * 
-     * example code:
-     * $simulation -> addSimulation(array('monty_id' => 1, 'behavior_matrix' => '{"Peter":35,"Ben":37,"Joe":43}', 'wins_switches' => 2, 
-     * 'total_switches' => 2, 'total_losses' => 2, 'total_wins' => 2, 'total_simulations' => 2));
+     * Example:
+     * Simulation::createSimulation(['monty_id' => 1, 'behavior_matrix' => '{"Always Switch": false}', 'wins_switched' => 40, 
+     *      'total_switches' => 50, 'total_losses' => 40, 'total_wins' => 60, 'total_simulations' => 100]);
      */
-    public function addSimulation($data){
-        $simulation = new Simulation();
-        $simulation -> monty_id = $data['monty_id'];
-        $simulation -> behavior_matrix = $data['behavior_matrix'];
-        $simulation -> wins_switches = $data['wins_switches'];
-        $simulation -> total_switches = $data['total_switches'];
-        $simulation -> total_losses = $data['total_losses'];
-        $simulation -> total_wins = $data['total_wins'];
-        $simulation -> total_simulations = $data['total_simulations'];
+    public static function createSimulation($data)
+    {
+        // Find if simulation already exists
+        $simulations = Simulation::where([
+            ['monty_id', '=', $data['monty_id']],
+        ])->get();
 
-        $subsimulation = $this -> where('monty_id', $simulation -> monty_id)
-        -> where( 'behavior_matrix', $simulation -> behavior_matrix) -> first();
-        if ($subsimulation != null){
-            $subsimulation -> wins_switches += $simulation -> wins_switches;
-            $subsimulation -> total_switches += $simulation -> total_switches;
-            $subsimulation -> total_losses += $simulation -> total_losses;
-            $subsimulation -> total_wins += $simulation -> total_wins;
-            $subsimulation -> total_simulations += $simulation -> total_simulations;
-            $subsimulation -> save();
-        }else{
-            $simulation -> save();
+        $simulation = NULL;
+
+        foreach ($simulations as $sim) {
+            if ($sim->behavior_matrix == $data['behavior_matrix']) {
+                $simulation = $sim;
+                break;
+            }     
         }
+
+        if ($simulation) {
+            $simulation->wins_switched += $data['wins_switched'];
+            $simulation->total_switches += $data['total_switches'];
+            $simulation->total_losses += $data['total_losses'];
+            $simulation->total_wins += $data['total_wins'];
+            $simulation->total_simulations += $data['total_simulations'];
+        } else {
+            $simulation = new Simulation();
+
+            $simulation->fill($data);
+            $simulation->monty_id = $data['monty_id'];
+        }
+
+        $simulation -> save();
+
+        return True;
     }
 }
