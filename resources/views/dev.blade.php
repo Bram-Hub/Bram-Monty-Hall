227bcc1197d2c54385b2171bee0ee0800ec2166e
Levyleader <55121623+Levyleader@users.noreply.github.com>
2021-07-29 05:25:17 +0800

    Update dev.blade.php

--- resources/views/dev.blade.php
+++ resources/views/dev.blade.php
@@ -5,14 +5,15 @@ use App\Models\Game;
 use App\Models\Cookie;
 use App\Models\Simulation;
 
-/*
-$monty = new Monty();
-$monty->type = "normal";
-$monty->total_wins = 0;
-$monty->total_losses = 0;
 
-$monty->save();
-*/
+// $monty = new Monty();
+// $monty->type = "normal";
+// $monty->total_wins = 0;
+// $monty->total_losses = 0;
+// $monty->switched_times = 0;
+// $monty -> total_games = 0;
+// $monty->save();
+
 
 //$monty = Monty::findOrFail(1);
 //echo($monty->type);
@@ -27,9 +28,9 @@ $monty->save();
 $monty = new Monty();
 $monty -> addMonty(array('type' => 'xx', 'total_wins' => 0, 'total_losses' => 0, 'switched_times' => 0, 'total_games' => 0));
 
-// $cookie = new Cookie();
-// $cookie->played = True;
-// $cookie -> save();
+$cookie = new Cookie();
+$cookie->played = True;
+$cookie -> save();
 $game = new Game();
 $game -> addGame(array('cookie_id' => 1, 'monty_id' => 1, 'outcome' => True, 'door_picked' => 1, 'door_opened' => 3, 'door_car' => 1));
 
@@ -39,11 +40,13 @@ $simulation -> addSimulation(array('monty_id' => 1, 'behavior_matrix' => '{"Pete
 $simulation -> addSimulation(array('monty_id' => 1, 'behavior_matrix' => '{"Peter":35,"Ben":37,"Joe":43}', 'wins_switches' => 2, 
 'total_switches' => 2, 'total_losses' => 2, 'total_wins' => 2, 'total_simulations' => 2));
 */
+$cookie = new Cookie();
+$cookie->played = True;
+$cookie -> save();
 
-
-//$dataGame = ['cookie_id' => 1, 'monty_id' => 1, 'door_picked' => 1, 'door_opened' => 2, 'door_car' => 3];
-//Game::createGame($dataGame);
-//Game::deleteGame(7);
+$dataGame = ['cookie_id' => 1, 'monty_id' => 1, 'door_picked' => 1, 'door_opened' => 2, 'door_car' => 3];
+Game::createGame($dataGame);
+// Game::deleteGame(7);
 
 $dataSim = ['monty_id' => 1, 'behavior_matrix' => '{"Always Switch": false}', 'wins_switched' => 40, 
             'total_switches' => 50, 'total_losses' => 40, 'total_wins' => 60, 'total_simulations' => 100];
