cdef2b412e1b0a3c0a147869ded4e823f667354a
AlexL374 <alexandernumber2@gmail.com>
2021-08-15 18:49:38 -0400

    Added research livewire component

---
+++ resources/views/livewire/research.blade.php
@@ -0,0 +1,17 @@
+<section id="game">
+    <h2 id="gameText">Pick a door.</h2>
+    <div class="center">
+        <button class="doorButton" id="door1btn">
+            <img src="img/closedDoor.png" alt="Door #1" class="door" id="door1img">
+        </button>
+        <button class="doorButton" id="door2btn">
+            <img src="img/closedDoor.png" alt="Door #2" class="door" id="door2img">
+        </button>
+        <button class="doorButton" id="door3btn">
+            <img src="img/closedDoor.png" alt="Door #3" class="door" id="door3img">
+        </button>
+        <br/>
+        <br/>
+        <br/>
+    </div>
+</section>
