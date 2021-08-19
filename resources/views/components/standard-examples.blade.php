25f6be6c2787f3620b661321c08794dfd03eb5bc
hsueha <hsueha@rpi.edu>
2021-08-19 15:51:19 -0400

    Updated styling and renamed classes

--- resources/views/components/standard-examples.blade.php
+++ resources/views/components/standard-examples.blade.php
@@ -1,119 +1,40 @@
 <head>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ URL::asset('css/education.css') }}" rel="stylesheet"/>
+    <link href="{{ URL::asset('css/standardExCarousel.css') }}" rel="stylesheet"/>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="{{ URL::asset('js/standardMontyEx.js') }}" type="text/javascript"></script>
 </head>
 <div class="standardMontyExamples">
-    <div class="carousel" aria-label="Gallery" onload="#carousel__slide1">
-        <ol class="carousel__viewport">
-            <li id="carousel__slide1"
-                tabindex="0"
-                class="carousel__slide">
-                <div class="carousel__snapper">
-                    <img class="carouselImg" src="img/educationPhotos/standardMontyExample1.png" alt="image">
-                    <a href="#carousel__slide7"
-                    class="carousel__prev">Go to last slide</a>
-                    <a href="#carousel__slide2"
-                    class="carousel__next">Go to next slide</a>
-                </div>
-            </li>
-            <li id="carousel__slide2"
-                tabindex="0"
-                class="carousel__slide">
-                <div class="carousel__snapper">
-                    <img class="carouselImg" src="img/educationPhotos/standardMontyExample2.png" alt="image">
-                    <a href="#carousel__slide1"
-                    class="carousel__prev">Go to previous slide</a>
-                    <a href="#carousel__slide3"
-                    class="carousel__next">Go to next slide</a>
-                </div>
-            </li>
-            <li id="carousel__slide3"
-                tabindex="0"
-                class="carousel__slide">
-                <div class="carousel__snapper">
-                    <img class="carouselImg" src="img/educationPhotos/standardMontyExample3.png" alt="image">
-                    <a href="#carousel__slide2"
-                    class="carousel__prev">Go to previous slide</a>
-                    <a href="#carousel__slide4"
-                    class="carousel__next">Go to next slide</a>
-                </div>
-            </li>
-            <li id="carousel__slide4"
-                tabindex="0"
-                class="carousel__slide">
-                <div class="carousel__snapper">
-                    <img class="carouselImg" src="img/educationPhotos/standardMontyExample4.png" alt="image">
-                    <a href="#carousel__slide3"
-                    class="carousel__prev">Go to previous slide</a>
-                    <a href="#carousel__slide5"
-                    class="carousel__next">Go to next slide</a>
-                </div>
-            </li>
-            <li id="carousel__slide5"
-                tabindex="0"
-                class="carousel__slide">
-                <div class="carousel__snapper">
-                    <img class="carouselImg" src="img/educationPhotos/standardMontyExample5.png" alt="image">
-                    <a href="#carousel__slide4"
-                    class="carousel__prev">Go to previous slide</a>
-                    <a href="#carousel__slide6"
-                    class="carousel__next">Go to next slide</a>
-                </div>
-            </li>
-            <li id="carousel__slide6"
-                tabindex="0"
-                class="carousel__slide">
-                <div class="carousel__snapper">
-                    <img class="carouselImg" src="img/educationPhotos/standardGraph.png" alt="image">
-                    <a href="#carousel__slide5"
-                    class="carousel__prev">Go to previous slide</a>
-                    <a href="#carousel__slide7"
-                    class="carousel__next">Go to next slide</a>
-                </div>
-            </li>
-            <li id="carousel__slide7"
-                tabindex="0"
-                class="carousel__slide">
-                <div class="carousel__snapper">
-                    <img class="carouselImg" src="img/educationPhotos/standardMonteCarloSim.png" alt="image">
-                    <a href="#carousel__slide6"
-                    class="carousel__prev">Go to previous slide</a>
-                    <a href="#carousel__slide1"
-                    class="carousel__next">Go to first slide</a>
-                </div>
-            </li>
-        </ol>
-        <aside class="carousel__navigation">
-            <ol class="carousel__navigation-list">
-                <li class="carousel__navigation-item">
-                    <a href="#carousel__slide1"
-                    class="carousel__navigation-button">Go to slide 1</a>
-                </li>
-                <li class="carousel__navigation-item">
-                    <a href="#carousel__slide2"
-                    class="carousel__navigation-button">Go to slide 2</a>
-                </li>
-                <li class="carousel__navigation-item">
-                    <a href="#carousel__slide3"
-                    class="carousel__navigation-button">Go to slide 3</a>
-                </li>
-                <li class="carousel__navigation-item">
-                    <a href="#carousel__slide4"
-                    class="carousel__navigation-button">Go to slide 4</a>
-                </li>
-                <li class="carousel__navigation-item">
-                    <a href="#carousel__slide5"
-                    class="carousel__navigation-button">Go to slide 5</a>
-                </li>
-                <li class="carousel__navigation-item">
-                    <a href="#carousel__slide6"
-                    class="carousel__navigation-button">Go to slide 6</a>
-                </li>
-                <li class="carousel__navigation-item">
-                    <a href="#carousel__slide7"
-                    class="carousel__navigation-button">Go to slide 7</a>
-                </li>
-            </ol>
-        </aside>
+    <div class="standardMontyExContent" onload="showDivs(1)">
+        <div class="mySlides default">
+            <img src="img/educationPhotos/standardMontyExample1.png" alt="image">
+            <div class="slideCounterLabel">1/7</div>
+        </div>
+        <div class="mySlides">
+            <img src="img/educationPhotos/standardMontyExample2.png" alt="image">
+            <div class="slideCounterLabel">2/7</div>
+        </div>
+        <div class="mySlides">
+            <img src="img/educationPhotos/standardMontyExample3.png" alt="image">
+            <div class="slideCounterLabel">3/7</div>
+        </div>
+        <div class="mySlides">
+            <img src="img/educationPhotos/standardMontyExample4.png" alt="image">
+            <div class="slideCounterLabel">4/7</div>
+        </div>
+        <div class="mySlides">
+            <img src="img/educationPhotos/standardMontyExample5.png" alt="image">
+            <div class="slideCounterLabel">5/7</div>
+        </div>
+        <div class="mySlides">
+            <img src="img/educationPhotos/standardGraph.png" alt="image">
+            <div class="slideCounterLabel">6/7</div>
+        </div>
+        <div class="mySlides">
+            <img src="img/educationPhotos/standardMonteCarloSim.png" alt="image">
+            <div class="slideCounterLabel">7/7</div>
+        </div>
+        <button class="prevSlideButton" onclick="plusDivs(-1)">&#10094;</button>
+        <button class="nextSlideButton" onclick="plusDivs(1)">&#10095;</button>
     </div>
 </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/game.js', 'public/js')
+    .js('resources/js/standardMontyEx.js', 'public/js')
     .postCss("resources/css/app.css", 'public/css', [
         require("tailwindcss"),
     ])
@@ -22,4 +23,5 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/footer.css', 'public/css')
     .postCss('resources/css/about.css', 'public/css')
     .postCss('resources/css/play.css', 'public/css')
-    .postCss('resources/css/research.css', 'public/css');
+    .postCss('resources/css/research.css', 'public/css')
+    .postCss('resources/css/standardExCarousel.css', 'public/css');
